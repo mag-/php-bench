d12ab03430e29afdbc240705181e86f80b4f8cff
Miëtek Bak <mietek@bak.io>
2014-08-12 15:49:09 +0100

    Use ByteString

--- scra.hs
+++ scra.hs
@@ -3,6 +3,8 @@ import Data.List          (delete, sortBy)
 import Data.Ord           (compare)
 import System.Environment (getArgs)
 
+import qualified Data.ByteString.Char8 as BS
+
 match :: String -> String -> Bool
 match []   _      = False
 match _    []     = True
@@ -10,18 +12,18 @@ match desk (w:ws) | elem w   desk = match (delete w   desk) ws
                   | elem '*' desk = match (delete '*' desk) ws
                   | otherwise     = False
 
-search :: String -> [String] -> [String]
-search = filter . match
+search :: String -> [BS.ByteString] -> [BS.ByteString]
+search desk words = filter (match desk . BS.unpack) words
 
-prettyPrint :: [String] -> IO ()
-prettyPrint words = mapM_ putStrLn $ sortBy compareLength words
-    where compareLength s1 s2 = compare (length s1) (length s2)
+prettyPrint :: [BS.ByteString] -> IO ()
+prettyPrint words = mapM_ BS.putStrLn $ sortBy compareLength words
+    where compareLength s1 s2 = compare (BS.length s1) (BS.length s2)
 
 main = do
     args  <- getArgs
-    words <- readFile "ODS6.txt"
+    words <- BS.readFile "ODS6.txt"
 
-    let dictionary = lines words
+    let dictionary = BS.lines words
         firstArg   = if length args == 1 then args !! 0
                                          else error "One parameter needed"
         results    = search (map toUpper firstArg) dictionary
